55c98880b15297934acc8ff98152b6c0ea511d25
Yenifer Tapia <yeni.flowerr23@gmail.com>
2022-10-03 12:16:33 -0400

    Added function values

---
+++ Functions/simple_functions.js
@@ -0,0 +1,20 @@
+//DEFINE THREE NAMED FUNCTIONS
+function favoriteThings(){
+    console.log("NAME: Yenifer Tapia");
+    console.log("MOVIE: Forest Gump");
+    console.log("MUSICIAN: Taylor Swift");
+    console.log("BAND: BTS");
+}
+
+function whyImHere(){
+    console.log("WHY: Travel and money");
+}
+
+function favoritePlace(){
+    console.log("Place: New York City");
+}
+
+//CALL ALL THREE FUNCTIONS BY NAME
+favoriteThings();
+whyImHere();
+favoritePlace();
